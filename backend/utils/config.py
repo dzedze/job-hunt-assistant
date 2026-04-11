96cf2343b817327b098c31e67c76fac36ced7ef8
Vinh-Phuc (Vince) Nguyen <phuc.nguyen.ufo@gmail.com>
2026-04-11 14:58:20 -0400

    chore: update config.py to create 'data' inside 'backend' instead

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -8,19 +8,17 @@ JOBS_API_KEY = os.getenv("JOBS_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 JOBS_API_HOST = os.getenv("JOBS_API_HOST")
 
-BASE_DIR = Path(".")  # current directory
-PARENT_DIR = (
-    BASE_DIR.parent.parent
-)  # parent of the current directory, which is the root of the project
+BASE_DIR = Path("backend")
+DATA_DIR = BASE_DIR / "data"
 
 SAVED_JOBS_DIR = (
-    PARENT_DIR / "data" / "jobs" / "saved"
+    DATA_DIR / "jobs" / "saved"
 )  # directory to save jobs
 TEMP_JOBS_DIR = (
-    PARENT_DIR / "data" / "jobs" / "temp"
+    DATA_DIR / "jobs" / "temp"
 )  # temporary directory for fetched jobs
 CUSTOMIZED_RESUMES_DIR = (
-    PARENT_DIR / "data" / "customized_resumes"
+    DATA_DIR / "customized_resumes"
 )  # directory to save customized resumes
 CUSTOMIZED_RESUMES_PATH = (
     CUSTOMIZED_RESUMES_DIR / "customized_resume.md"
@@ -32,17 +30,17 @@ CUSTOMIZED_COVER_LETTER_PDF_PATH = (
     CUSTOMIZED_RESUMES_DIR / "cover_letter.pdf"
 )
 CUSTOMIZED_MESSAGES_DIR = (
-    PARENT_DIR / "data" / "customized_messages"
+    DATA_DIR / "customized_messages"
 )  # directory to save customized messages
 CUSTOMIZED_MESSAGES_PATH = (
     CUSTOMIZED_MESSAGES_DIR / "customized_message.txt"
 )  # path to the customized message
-JOBS_REPORTS_DIR = PARENT_DIR / "data" / "reports"
+JOBS_REPORTS_DIR = DATA_DIR / "reports"
 JOBS_REPORTS_PATH = (
     JOBS_REPORTS_DIR / "report.md"
 )  # path to the jobs report
 RESUME_PATH = (
-    PARENT_DIR / "data" / "resume" / "resume.pdf"
+    DATA_DIR / "resume" / "resume.pdf"
 )  # path to the resume
 TEMP_FETCHED_JOBS_PATH = TEMP_JOBS_DIR / "temp_fetched_jobs.json"
 
